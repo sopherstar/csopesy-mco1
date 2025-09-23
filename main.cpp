42b598e1af16519f72e0ebd2d51a9489eda45154
sosophia <s.ilustre@gmail.com>
2025-09-23 17:30:21 +0800

    first

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <iomanip>
+#include <windows.h>
+
+//main thread
+
+//command interpreter
+//keyboard handler
+//display handler
+//marquee logic
